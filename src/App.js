23be65438fc2c1f7ccca365724094d23ea148328
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-11 20:09:50 -0700

    Display Items

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([])
+  return (
+    <main>
+      <section className='menu section'>
+        <h2>Our menu</h2>
+        <div className="underline"></div>
+        <Categories />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
