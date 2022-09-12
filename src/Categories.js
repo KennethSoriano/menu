4c8dec2f67bbbba53e94663306389902ab65c8ee
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-11 21:19:02 -0700

    all button

--- src/Categories.js
+++ src/Categories.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Categories = ({filterItems}) => {
   return (
     <div className="btn-container">
-        <button className='filter-btn' onClick={() => filterItems('breakfast')}>Breakfast</button>
+        <button className='filter-btn' onClick={() => filterItems('breakfast')}>
+            Breakfast
+        </button>
+        <button className='filter-btn' onClick={() => filterItems('all')}>
+            All
+        </button>
     </div>
   );
 };
