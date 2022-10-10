b067042ef4d8fbf376cbb52828598f0e8c243e42
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-09 18:51:06 -0700

    redo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))] 
+
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setcategories] = useState([]);
+  const [categories, setcategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
+
   return (
     <main>
      <section className="menu section">
@@ -14,7 +26,7 @@ function App() {
         <h2>Our Menu</h2>
         <div className="underline"></div>
       </div>
-      <Categories />
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems}/>
      </section>
     </main>
--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
+const Categories = ({filterItems, categories}) => {
   return (
     <div className="btn-container">
-      
+      {
+        categories.map((category, index) => {
+          return (
+            <button 
+              type='button' 
+              className='filter-btn' 
+              key={index} 
+              onClick={() => filterItems(category)}
+            >
+              {category}
+            </button>
+          )
+        })
+      }
     </div>
   );
 };
